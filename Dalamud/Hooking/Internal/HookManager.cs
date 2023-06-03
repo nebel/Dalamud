f9e111ff3ed903ce2ba1d08dfb864fa499a6d853
nebel <nebel@users.noreply.github.com>
2023-06-03 22:44:23 +0900

    Improve unhooking of hooked functions

--- Dalamud/Hooking/Internal/HookManager.cs
+++ Dalamud/Hooking/Internal/HookManager.cs
@@ -1,7 +1,12 @@
+#define UNHOOKER_DBG
+
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+#if UNHOOKER_DBG
+using System.Runtime.CompilerServices;
+#endif
 using System.Runtime.InteropServices;
 
 using Dalamud.Logging.Internal;
@@ -34,9 +39,36 @@ internal class HookManager : IDisposable, IServiceType
     internal static ConcurrentDictionary<Guid, HookInfo> TrackedHooks { get; } = new();
 
     /// <summary>
-    /// Gets a static dictionary of original code for a hooked address.
+    /// Gets a static dictionary of unhookers for a hooked address.
     /// </summary>
-    internal static ConcurrentDictionary<IntPtr, byte[]> Originals { get; } = new();
+    internal static ConcurrentDictionary<IntPtr, Unhooker> Unhookers { get; } = new();
+
+#if UNHOOKER_DBG
+    /// <summary>
+    /// Creates a new Unhooker instance for the provided address if no such unhooker was already registered, or returns
+    /// an existing instance if the address registered previously.
+    /// </summary>
+    /// <param name="address">The address of the instruction.</param>
+    /// <param name="sourceFilePath">The source path of the caller (automatically filled).</param>
+    /// <returns>A new Unhooker instance.</returns>
+    public static Unhooker RegisterUnhooker(IntPtr address, [CallerFilePath] string sourceFilePath = "")
+    {
+        Log.Verbose($"Registering hook at 0x{address.ToInt64():X} <${sourceFilePath}>");
+        return Unhookers.GetOrAdd(address, adr => new Unhooker(adr, sourceFilePath));
+    }
+#else
+    /// <summary>
+    /// Creates a new Unhooker instance for the provided address if no such unhooker was already registered, or returns
+    /// an existing instance if the address registered previously.
+    /// </summary>
+    /// <param name="address">The address of the instruction.</param>
+    /// <returns>A new Unhooker instance.</returns>
+    public static Unhooker RegisterUnhooker(IntPtr address)
+    {
+        Log.Verbose($"Registering hook at 0x{address.ToInt64():X}");
+        return Unhookers.GetOrAdd(address, adr => new Unhooker(adr));
+    }
+#endif
 
     /// <summary>
     /// Gets a static dictionary of the number of hooks on a given address.
@@ -48,7 +80,7 @@ internal class HookManager : IDisposable, IServiceType
     {
         RevertHooks();
         TrackedHooks.Clear();
-        Originals.Clear();
+        Unhookers.Clear();
     }
 
     /// <summary>
@@ -60,7 +92,7 @@ internal class HookManager : IDisposable, IServiceType
     {
         while (true)
         {
-            var hasOtherHooks = HookManager.Originals.ContainsKey(address);
+            var hasOtherHooks = HookManager.Unhookers.ContainsKey(address);
             if (hasOtherHooks)
             {
                 // This address has been hooked already. Do not follow a jmp into a trampoline of our own making.
@@ -124,28 +156,11 @@ internal class HookManager : IDisposable, IServiceType
         return address;
     }
 
-    private static unsafe void RevertHooks()
+    private static void RevertHooks()
     {
-        foreach (var (address, originalBytes) in Originals)
+        foreach (var unhooker in Unhookers.Values)
         {
-            var i = 0;
-            var current = (byte*)address;
-            // Find how many bytes have been modified by comparing to the saved original
-            for (; i < originalBytes.Length; i++)
-            {
-                if (current[i] == originalBytes[i])
-                    break;
-            }
-
-            var snippet = originalBytes[0..i];
-
-            if (i > 0)
-            {
-                Log.Verbose($"Reverting hook at 0x{address.ToInt64():X} ({snippet.Length} bytes)");
-                MemoryHelper.ChangePermission(address, i, MemoryProtection.ExecuteReadWrite, out var oldPermissions);
-                MemoryHelper.WriteRaw(address, snippet);
-                MemoryHelper.ChangePermission(address, i, oldPermissions);
-            }
+            unhooker.Unhook();
         }
     }
 }
---
+++ Dalamud/Hooking/Internal/Unhooker.cs
@@ -0,0 +1,133 @@
+﻿#define UNHOOKER_DBG
+
+using System;
+
+using Dalamud.Logging.Internal;
+using Dalamud.Memory;
+
+namespace Dalamud.Hooking.Internal;
+
+/// <summary>
+/// A class which stores a copy of the bytes at a location which will be hooked in the future, such that those bytes can
+/// be restored later to "unhook" the function.
+/// </summary>
+public class Unhooker
+{
+    private static readonly ModuleLog Log = new("UH");
+
+    private readonly IntPtr address;
+#if UNHOOKER_DBG
+    private readonly string caller;
+#endif
+    private byte[] originalBytes;
+    private bool trimmed;
+
+#if UNHOOKER_DBG
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Unhooker"/> class. Upon creation, the Unhooker stores a copy of
+    /// the bytes stored at the provided address, and can be used to restore these bytes when the hook should be
+    /// removed. As such this class should be instantiated before the function is actually hooked.
+    /// </summary>
+    /// <param name="address">The address which will be hooked.</param>
+    /// <param name="sourceFilePath">The source path of the caller.</param>
+    public Unhooker(IntPtr address, string sourceFilePath = "")
+    {
+        this.caller = sourceFilePath;
+        this.address = address;
+        MemoryHelper.ReadRaw(address, 0x32, out this.originalBytes);
+    }
+#else
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Unhooker"/> class. Upon creation, the Unhooker stores a copy of
+    /// the bytes stored at the provided address, and can be used to restore these bytes when the hook should be
+    /// removed. As such this class should be instantiated before the function is actually hooked.
+    /// </summary>
+    /// <param name="address">The address which will be hooked.</param>
+    public Unhooker(IntPtr address)
+    {
+        this.address = address;
+        MemoryHelper.ReadRaw(address, 0x32, out this.originalBytes);
+    }
+#endif
+
+    /// <summary>
+    /// When called after a hook is created, checks the pre-hook original bytes and post-hook modified bytes, trimming
+    /// the original bytes stored and removing unmodified bytes from the end of the byte sequence. Assuming no
+    /// concurrent actions modified the same address space, this should result in storing only the minimum bytes
+    /// required to unhook the function.
+    /// </summary>
+    public void TrimAfterHook()
+    {
+        if (this.trimmed)
+        {
+            return;
+        }
+
+#if UNHOOKER_DBG
+        var naiveLength = this.GetNaiveHookLength();
+        var fullLength = this.GetFullHookLength();
+        var status = fullLength == naiveLength ? "HOOK-OK" : "HOOK-MISMATCH";
+        Log.Verbose(
+            $"Trimmed 0x{this.address.ToInt64():X}: naiveLength={naiveLength} fullLength={fullLength} <{this.caller}> ({status})");
+
+        if (status == "HOOK-MISMATCH")
+        {
+            MemoryHelper.ReadRaw(this.address, this.originalBytes.Length, out var newBytes);
+            Log.Verbose("Mismatch analysis:");
+            Log.Verbose($"      Original: {Convert.ToHexString(this.originalBytes)}");
+            Log.Verbose($"        Hooked: {Convert.ToHexString(newBytes)}");
+            Log.Verbose($"  Naive Unhook: {Convert.ToHexString(this.originalBytes)[..(naiveLength * 2)]}");
+            Log.Verbose($"   Full Unhook: {Convert.ToHexString(this.originalBytes)[..(fullLength * 2)]}");
+        }
+#endif
+
+        this.originalBytes = this.originalBytes[..this.GetFullHookLength()];
+        this.trimmed = true;
+    }
+
+    /// <summary>
+    /// Attempts to unhook the function by replacing the hooked bytes with the original bytes. If
+    /// <see cref="TrimAfterHook"/> was called, the trimmed original bytes stored at that time will be used for
+    /// unhooking. Otherwise, a naive algorithm which only restores bytes until the first unchanged byte will be used in
+    /// order to avoid overwriting adjacent data.
+    /// </summary>
+    public void Unhook()
+    {
+        var len = this.trimmed ? this.originalBytes.Length : this.GetNaiveHookLength();
+        if (len > 0)
+        {
+            Log.Verbose($"Reverting hook at 0x{this.address.ToInt64():X} ({len} bytes, trimmed={this.trimmed})");
+            MemoryHelper.ChangePermission(this.address, len, MemoryProtection.ExecuteReadWrite, out var oldPermissions);
+            MemoryHelper.WriteRaw(this.address, this.originalBytes[..len]);
+            MemoryHelper.ChangePermission(this.address, len, oldPermissions);
+        }
+    }
+
+    private unsafe int GetNaiveHookLength()
+    {
+        var current = (byte*)this.address;
+        for (var i = 0; i < this.originalBytes.Length; i++)
+        {
+            if (current[i] == this.originalBytes[i])
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    private unsafe int GetFullHookLength()
+    {
+        var current = (byte*)this.address;
+        for (var i = this.originalBytes.Length - 1; i >= 0; i--)
+        {
+            if (current[i] != this.originalBytes[i])
+            {
+                return i + 1;
+            }
+        }
+
+        return 0;
+    }
+}
